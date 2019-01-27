67f0fb853631bdfe74c02f537fc0a93323ef7a10
dderevjanik <daniel.derevjanik@gmail.com>
2019-01-27 01:25:41 +0100

    Working properly

--- index.test.ts
+++ index.test.ts
@@ -1,93 +1,122 @@
 import { h } from "./";
 import { Hsml } from "prest-lib/dist/hsml";
 
-const onlyTag = h("div#app");
-const tagWithText = h("p", "Hello World");
-const tagWithSeveralText = h("p", "Lorem\n", "Ipsum\n");
+/**
+ * Utils
+ */
 
-const tagWithHsml = h("div.app",
-    h("p", "paragraph"));
-const tagWith2Hsml = h("section.app",
-    h("p", "paragraph"),
-    h("p", "another paragraph"));
-const tagWithHsmlAndText = h("div#app",
-    "this is my app",
-    h("p", "paragraph"),
-    "footer text");
-
-const tagWithAttrs = h("div#app", { class: "app" });
-const tagWithAttrsText = h("div#app", { class: "app" }, "special");
-const tagWithAttrsHsml = h("div#app", { class: "app" },
-    h("p", "paragraph"));
-const tagWithAttrsTextHsml = h("div#app", { class: "app" },
-    "Header text",
-    h("p", "paragraph", h("b", "strong")),
-    "Footer text");
-
-const todo = (text: string): Hsml => h("li",
-    h("a", { href: "#" }, text));
-
-const complex = h("div#todo-app",
-    h("h2", "Todo App"),
-    h("div.main",
-        h("input.input-text", { type: "checkbox" }, "placeholder"),
-        h("button", { onclick: ["click" ]}),
-        h("ul.todos",
-            ...["first", "second", "third"].map(txt => todo(txt)))));
-
-// Tests
-
-function isEqual(value: Hsml, expect: Hsml) {
-    console.log("val:", JSON.stringify(value, null, 2))
-    console.log("exp:", JSON.stringify(expect, null, 2));
-    return JSON.stringify(value) === JSON.stringify(expect);
+function prettify(hsml: any): string {
+    return JSON.stringify(hsml, null, 4)
+    .replace(/\[\s+"/mg, `["`)
+    .replace(/",\s+"(.*)"\s+]/mg, ', "$1"]')
+    .replace(/,\s+\[(\s+)\[/mg, ', [$1[');
 }
 
-console.assert(isEqual(onlyTag, ["div#app"]));
-console.assert(isEqual(tagWithText, ["p", "Hello World"]));
-console.assert(isEqual(tagWithSeveralText, ["p", ["Lorem\n", "Ipsum\n"]]));
-console.assert(isEqual(tagWithHsml, ["div.app", ["p", "paragraph"]]));
-console.assert(isEqual(tagWith2Hsml, ["section.app", [
-    ["p", "paragraph"],
-    ["p", "another paragraph"]
-]]));
-console.assert(isEqual(tagWithHsmlAndText, ["div#app", [
-    "this is my app",
-    ["p", "paragraph"],
-    "footer text"
-]]));
-console.assert(isEqual(tagWithHsmlAndText, ["div#app", [
-    "this is my app",
-    ["p", "paragraph"],
-    "footer text"
-]]));
-console.assert(isEqual(tagWithAttrs, ["div#app", { class: "app" }]));
-console.assert(isEqual(tagWithAttrsText, ["div#app", { class: "app"},
-    "special"
-]));
-console.assert(isEqual(tagWithAttrsHsml, ["div#app", { class: "app" }, [
-    ["p", "paragraph"]
-]]));
-console.assert(isEqual(tagWithAttrsTextHsml, ["div#app", { class: "app" }, [
-    "Header text",
-    ["p", [
-        "paragraph",
-        ["b", "strong"]
-    ]],
-    "Footer text"
-]]));
-console.assert(isEqual(complex, ["div#todo-app", [
-    ["h2", "Todo App"],
-    ["div.main", [
-        ["input.input-text", { type: "checkbox" }, "placeholder"],
-        ["button", { onclick: ["click" ]}],
-        ["ul.todos", [
-            ["li",
-                [["a", { href: "#" }, "first"]]],
-            ["li",
-                [["a", { href: "#" }, "second"]]],
-            ["li",
-                [["a", { href: "#" }, "third"]]]
-        ]] as any
-    ]]
-]]));
+function isEqual(txt: string, current: Hsml, expect: Hsml): void {
+    const equal = JSON.stringify(current) === JSON.stringify(expect);
+    if (!equal) {
+        console.error(txt);
+        console.log("current:", prettify(current));
+        console.log("expect :", prettify(expect));
+    }
+}
+
+/**
+ * Tests
+ */
+
+isEqual("Tag Only",
+    h("div#app"),
+    ["div#app"]
+);
+isEqual("HSML element with text",
+    h("p", "Hello World"),
+    ["p", "Hello World"]
+);
+isEqual("HSML element with two text child",
+    h("p", "Lorem\n", "Ipsum\n"),
+    ["p", ["Lorem\n", "Ipsum\n"]]
+);
+isEqual("Nested HSML element with text",
+    h("div.app", h("p", "paragraph")),
+    ["div.app", [
+        ["p", "paragraph"]]]
+);
+isEqual("Two nested HSML elmeent with text",
+    h("section.app",
+        h("p", "paragraph"),
+        h("p", "another paragraph")),
+    ["section.app", [
+        ["p", "paragraph"],
+        ["p", "another paragraph"]]]
+);
+isEqual("Nested Text and HSML element",
+    h("div#app",
+        "this is my app",
+        h("p", "paragraph"),
+        "footer text"),
+    ["div#app", [
+        "this is my app",
+        ["p", "paragraph"],
+        "footer text"]]
+);
+isEqual("Attributes",
+    h("div#app", { class: "app" }),
+    ["div#app", { class: "app" }]
+);
+isEqual("Attributes with text",
+    h("div#app", { class: "app" },
+        "special"),
+    ["div#app", { class: "app"},
+        "special"]
+);
+isEqual("Nested HSML with attributes and text",
+    h("span",
+        h("a", { href: "#" }, "textnoed")),
+    ["span", [
+        ["a", { href: "#" }, "textnoed"]]]
+);
+isEqual("Nested HSML with only text",
+    h("div#app", { class: "app" },
+        h("p", "paragraph")),
+    ["div#app", { class: "app" }, [
+        ["p", "paragraph"]]]
+);
+isEqual("Complex scenario",
+    h("div#app", { class: "app" },
+        "Header text",
+        h("p", "paragraph", h("b", "strong")),
+        "Footer text"),
+    ["div#app", { class: "app" }, [
+        "Header text",
+        ["p", [
+            "paragraph",
+            ["b", "strong"]
+        ]],
+        "Footer text"]]
+);
+const todo = (text: string): Hsml => h("li",
+    h("a", { href: "#" }, text));
+isEqual("Todo App",
+    h("div#todo-app",
+        h("h2", "Todo App"),
+        h("div.main",
+            h("input.input-text", { type: "checkbox" }, "placeholder"),
+            h("button", { onclick: ["click" ]}),
+            h("ul.todos",
+                ...["first", "second", "third"].map(txt => todo(txt))))),
+    ["div#todo-app", [
+        ["h2", "Todo App"],
+        ["div.main", [
+            ["input.input-text", { type: "checkbox" }, "placeholder"],
+            ["button", { onclick: ["click" ]}],
+            ["ul.todos", [
+                ["li",
+                    [["a", { href: "#" }, "first"]]],
+                ["li",
+                    [["a", { href: "#" }, "second"]]],
+                ["li",
+                    [["a", { href: "#" }, "third"]]]
+            ]] as any
+        ]]]]
+);
--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import { Hsml, HsmlAttrs } from "prest-lib/dist/hsml";
 
-function isHsmlEl(item: any): item is Hsml {
+function isPrimitive(item: any): item is number | string | boolean {
     return (typeof item === "string")
+        || (typeof item === "number")
+        || (typeof item === "boolean");
+}
+
+function isHsmlEl(item: any): item is Hsml {
+    return isPrimitive(item)
         || (typeof item === "function")
         || (typeof item === "object" && item["render"])
         || Array.isArray(item)
@@ -12,18 +18,20 @@ function isHsmlEl(item: any): item is Hsml {
 export function h(tag: string, ...childs: Hsml[]): Hsml;
 export function h(tag: string, attr: HsmlAttrs, ...childs: Hsml[]): Hsml;
 export function h(tag: string, attr?: HsmlAttrs | Hsml, ...child: Hsml[]): Hsml {
-    console.log(tag, attr, child);
     if (attr) {
+        const hasChild = child && child.length;
+        const isChildText = hasChild && (child.length === 1) && isPrimitive(child[0]);
+        const isAttrText = isPrimitive(attr);
         if (isHsmlEl(attr)) {
             // attr = hsml obj
-            return [tag, (child && child.length)
+            return [tag, hasChild
                     ? [attr, ...child]
-                    : attr] as any;
+                    : isAttrText ? attr : [attr]] as any;
         } else {
             // attr = attributes
             const result = [tag, attr] as any[];
-            if (child && child.length) {
-                result.push(child.length === 1 && (typeof child[0] === "string")
+            if (hasChild) {
+                result.push(isChildText
                     ? child[0]
                     : child);
             }
